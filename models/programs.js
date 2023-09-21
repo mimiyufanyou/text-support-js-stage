0c80278617c762f148b3b43257856af0d1876e50
mimiyufanyou <yufan.you@gmail.com>
2023-09-21 13:00:06 -0700

    programs

---
+++ models/programs.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+// Define the program schema
+const programSchema = new mongoose.Schema({
+  programId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: String,
+    required: true
+  },
+  quizzes: [quizSchema],
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Add a pre-save hook to update the "updatedAt" field
+programSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+// Create the model
+const Program = mongoose.model('Program', programSchema);
+
+// Export the models
+module.exports = {
+  Program
+};
