4f16e3902f71bdf76bf64f9e62938cef315617a4
mimiyufanyou <yufan.you@gmail.com>
2023-09-28 21:06:11 -0700

    completely shorten it.

---
+++ config/parking_lot_prompts.js
@@ -0,0 +1,29 @@
+`"parking_lot": [
+"actionItems": [
+    "First action for assistant",
+    "Second action for assistant",
+    "Third action for assistant"
+],
+"quotesOfNote": [
+    "First notable quote from the user",
+    "Second notable quote from the user",
+    "Third notable quote from the user"
+],
+"dyadicAnalysis": [
+    "entity1_entity2": "emotion1 ↔️ emotion2",
+    "entity3_entity4": "emotion3 ↔️ emotion4"
+],
+"complexity_score": [
+    "Sentence Structure": "Complex",
+    "Vocabulary": "Academic and specialized",
+    "Conceptual Depth": "High",
+    "Coherence": "Requires prior knowledge",
+    "Overall Complexity Score": "High"
+],
+"urgency_score": [
+    "Urgency": "Very High",
+    "EmotionalTone": "Very High",
+    "Complexity": "Very High",
+    "Length": "Very High",
+    "InformationDensity": "Very High"
+]`
--- config/system_prompts.js
+++ config/system_prompts.js
@@ -49,34 +49,6 @@ Summarize the conversation history in a JSON object with this exact format:
         "Second takeaway",
         "Third takeaway"
     ],
-    "actionItems": [
-        "First action for assistant",
-        "Second action for assistant",
-        "Third action for assistant"
-    ],
-    "quotesOfNote": [
-        "First notable quote from the user",
-        "Second notable quote from the user",
-        "Third notable quote from the user"
-    ],
-    "dyadicAnalysis": [
-        "entity1_entity2": "emotion1 ↔️ emotion2",
-        "entity3_entity4": "emotion3 ↔️ emotion4"
-    ],
-    "complexity_score": [
-        "Sentence Structure": "Complex",
-        "Vocabulary": "Academic and specialized",
-        "Conceptual Depth": "High",
-        "Coherence": "Requires prior knowledge",
-        "Overall Complexity Score": "High"
-    ],
-    "urgency_score": [
-        "Urgency": "Very High",
-        "EmotionalTone": "Very High",
-        "Complexity": "Very High",
-        "Length": "Very High",
-        "InformationDensity": "Very High"
-    ]
 }
 `
 const EARL = 
