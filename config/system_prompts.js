b708a91e99546cde5ccc65ddd1eaaa62c17b165a
mimiyufanyou <yufan.you@gmail.com>
2023-10-11 14:51:28 -0700

    change system prompt mandatory follow-up

--- config/system_prompts.js
+++ config/system_prompts.js
@@ -93,7 +93,7 @@ Summarize the chat history in a JSON string with this exact format. No other out
   keyTakeaways: Array with 3 bullet key takeaways,
   actionItems: Array with 3 bullet action items for assistant,
   quotesOfNote: Array with 3 bullet quotes of note only from the user
-  frequencyOfFollowUp: String with human interval format for next follow-up (e.g., "15 minutes", "1 hour", "1 day", "1 week", "1 month", "1 year")
+  frequencyOfFollowUp: Cannot be null or empty. String with human-interval format for next follow-up (e.g., "15 minutes", "1 hour", "1 day", "1 week")
 }`;
 
 
